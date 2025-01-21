ed5549b58d39387a1080784e99533988a642b590
harshita <harshita.jain246@gmail.com>
2025-01-21 12:13:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from dotenv import load_dotenv 
+load_dotenv()
+import google.generativeai as genai
+
+import os
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title='chatbot for Q&A')
+st.header('chatbot for Q&A by hj')
+
+input = st.text_input("Input :" ,key = "input")
+
+submit = st.button("Get Response!")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("the response is:")
+    st.write(response)
+
+
+
+
+
